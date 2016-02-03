8283f46020b85108d3019b4cd5ae9f97b353516e
Andre Fabiano Lermen <lermen.andrefabiano@gmail.com>
2016-02-03 15:12:25 -0200

    especialidades

--- homehelp/WebContent/especialidade.js
+++ homehelp/WebContent/especialidade.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
 App.Modulos.Especialidade = {
 	init : function() {
 		App.Modulos.Especialidade.listar();
+		$('#btnBuscarEspecialidade').on('click', function(){
+			$(this).button('loading');	
+			var especialidade = $('#txtBuscarEspecialidade').val();
+			App.Modulos.Especialidade.getEspecialidades(especialidade);
+		});
 	},
 	listar : function() {
 		console.log('listar especialidades');
@@ -37,19 +42,38 @@ App.Modulos.Especialidade = {
 			
 			$('.list-group a').each(function(index){
 				$(this).on('click', function(){
-					console.log('.list-group a', this);		
-					var id = $(this).data('id');
-					self.excluir(id);			
+					console.log('.list-group a', this);					
+					self.populaModalExcluir(this);			
 				});					
 			});				
 		}else{
 			bootbox.alert('Não foram encontradas especialidades.\nPor favor, refaça sua pesquisa.');
 		}		
 	},	
+	populaModalExcluir : function($a) {			
+		var id = $($a).data('id');	
+		var esp = $($a).data('esp');
+		
+		var especialidadeSel = {
+			descricao : esp,
+			id : id
+		};
+		
+		var template = Handlebars.compile($("#modal-excluir-template").html());			
+		$(".modal-content").html(template(especialidadeSel));	
+		
+		App.Modulos.Especialidade.especialidadeSel = especialidadeSel;
+		
+		$('#btnExcluir').on('click', function(){
+			$(this).button('loading');	
+			App.Modulos.Especialidade.excluir(id);
+		});				
+		
+	},
 	excluir : function(id) {
 		var self = this;	
 		
-		 $.ajax({
+		$.ajax({
 			type : 'GET',			
 			url : $.ajaxSetup().urlBase + 'especialidade/excluir?usuarioEspecialidadeId='+id	
 		}).done(function(result){			
@@ -61,5 +85,86 @@ App.Modulos.Especialidade = {
 		}).always(function() {
 			console.log('always');
 		});				
+	},
+	getEspecialidades : function(especialidade) {
+		console.log('getEspecialidades');
+		var self = this;	
+		
+		 $.ajax({
+			type : 'GET',			
+			url : $.ajaxSetup().urlBase + 'especialidade/getEspecialidades?especialidade='+especialidade	
+		}).done(function(result){
+			self.renderGetEspecialidades(result);			
+		}).fail(function(xhr, type){			
+			bootbox.alert('Sistema indisponível.\nPor favor, tente novamente mais tarde.', function(){
+				$('#btnBuscarEspecialidade').button('reset');
+			});			
+		}).always(function() {
+			console.log('always');
+		});		
+	},
+	renderGetEspecialidades : function(result) {		
+		console.log('renderGetEspecialidades', result);
+		
+		var self = this;
+		
+		if(result!=undefined){			
+			var template = Handlebars.compile($("#get-especialidades-template").html());
+			
+			$("#bloco-especialidades").html(template(result));
+			
+			$('#bloco-especialidades a').each(function(index){
+				$(this).on('click', function(){
+					console.log('.list-group a', this);		
+					App.Modulos.Especialidade.populaModalEsp(this);			
+				});					
+			});				
+		}		
+		$('#btnBuscarEspecialidade').button('reset');
+	},	
+	populaModalEsp : function($a) {			
+		var id = $($a).data('id');	
+		var esp = $($a).data('esp');
+		
+		var especialidadeSel = {
+			descricao : esp,
+			id : id
+		};
+		
+		var template = Handlebars.compile($("#modal-esp-template").html());			
+		$(".modal-content").html(template(especialidadeSel));	
+		
+		App.Modulos.Especialidade.especialidadeSel = especialidadeSel;
+		
+		$('#btnAdd').on('click', function(){
+			$(this).button('loading');	
+			App.Modulos.Especialidade.incluir();
+		});				
+		
+	},
+	incluir : function() {
+		console.log('incluir especialidade');
+		var self = this;	
+		
+		var login = JSON.parse(localStorage.getItem('login'));		
+		var usuarioId = login.id;				
+		var especialidadeId = App.Modulos.Especialidade.especialidadeSel.id;
+		var valorCobrado = $('#txtValorCobrado').val();	
+		
+		 $.ajax({
+			type : 'POST',
+			url : $.ajaxSetup().urlBase + 'especialidade/incluir?usuarioId='+usuarioId+'&especialidadeId='+especialidadeId+'&valorCobrado='+valorCobrado			
+		}).done(function(result){
+			bootbox.alert('Sucesso ao adicionar especialidade.\nObrigado!.', function(){
+				$('#btnAdd').button('reset');
+				App.Modulos.Especialidade.listar();
+			})		
+		}).fail(function(xhr, type){
+			bootbox.alert('Sistema indisponível.\nPor favor, tente novamente mais tarde.', function(){
+				$('#btnAdd').button('reset');
+			})			
+		}).always(function() {
+			console.log('always');
+		});
 	}
 };
--- homehelp/src/main/java/br/com/home/help/web/EspecialidadeResource.java
+++ homehelp/src/main/java/br/com/home/help/web/EspecialidadeResource.java
@@ -7,13 +7,11 @@ import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.dozer.Mapper;
 
